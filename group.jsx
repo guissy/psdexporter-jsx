fe56a826a676d136497ab64f9d829b6690fbffe9
FlashFrog <guissy@163.com>
2016-05-30 00:57:10 +0800

    fixed same name

---
+++ group.jsx
@@ -0,0 +1,15 @@
+function(item){
+    var layers = item.artLayers;
+    for(var i =0 ;i < layers.length; i++){
+        if(item.name.indexOf('')) {
+        }
+    }
+}
+
+
+
+
+
+
+
+resolve(activeDocument);
